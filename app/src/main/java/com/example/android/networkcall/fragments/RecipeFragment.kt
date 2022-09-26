0a3c6797507bdf8b32d895e8830271652b220129
Sagar <sagar.0dev@gmail.com>
2022-09-27 00:55:00 +0530

    navigation in compose

--- app/src/main/java/com/example/android/networkcall/fragments/RecipeFragment.kt
+++ app/src/main/java/com/example/android/networkcall/fragments/RecipeFragment.kt
@@ -4,22 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 import com.example.android.networkcall.R
 
-class RecipeListFragment : Fragment() {
-
+class RecipeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -29,24 +34,15 @@ class RecipeListFragment : Fragment() {
             setContent {
                 Column(modifier = Modifier.padding(16.dp)) {
                     Text(
-                        text = "RecipeList",
+                        text = "This is Recipe Fragment",
                         style = TextStyle(
                             fontSize = 21.sp
                         )
                     )
-                    Spacer(modifier = Modifier.padding(10.dp))
-                    Button(
-                        onClick = {
-                            findNavController().navigate(R.id.action_recipeListFragment_to_recipeFragment)
-                        }
-                    ) {
-                        Text(text = "TO RECIPE FRAGMENT")
-                    }
                 }
             }
         }
 
         return view
     }
-
 }
